a9bcd93e7576b59af1717a4bec99e003cc0f7e12
Michal Banik <banik.michal.sk@gmail.com>
2023-09-10 22:19:16 +0200

    refactor to dynamically run method provided as parameter

--- xUnit/src/main/java/dev/banik/xunit/TestCase.java
+++ xUnit/src/main/java/dev/banik/xunit/TestCase.java
@@ -1,5 +1,8 @@
 package dev.banik.xunit;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class TestCase {
     public static void main(String[] args) {
 //        TestCase("testMethod").run();
@@ -13,9 +16,15 @@ public class TestCase {
 
     static class WasRun implements Runnable{
 
+        private final Method method;
         public boolean wasRun = false;
 
         public WasRun(String methodName) {
+            try {
+                this.method = this.getClass().getMethod(methodName);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         public void testMethod() {
@@ -24,7 +33,11 @@ public class TestCase {
 
         @Override
         public void run() {
-            testMethod();
+            try {
+                method.invoke(this);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
