f2f74fc5b5e87bcd8b0d960b78eaf6ce75f56b70
Michal Banik <banik.michal.sk@gmail.com>
2023-09-18 16:08:28 +0200

    remove redundant variable wasSetUp

--- xUnit/src/main/java/dev/banik/xunit/WasRun.java
+++ xUnit/src/main/java/dev/banik/xunit/WasRun.java
@@ -3,7 +3,6 @@ package dev.banik.xunit;
 class WasRun extends TestCase {
 
     public boolean wasRun = false;
-    public boolean wasSetUp;
     public String log = "";
 
     public WasRun(String methodName) {
@@ -13,7 +12,6 @@ class WasRun extends TestCase {
     @Override
     protected void setUp() {
         this.wasRun = false;
-        this.wasSetUp = true;
         this.log = "setUp ";
     }
 
