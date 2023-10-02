4ef68f6930ae1d0a2a9ecbebf7a020f6d67dfc26
Michal Banik <banik.michal.sk@gmail.com>
2023-10-02 23:13:58 +0200

    remove redundant wasRun property

--- xUnit/src/main/java/dev/banik/xunit/WasRun.java
+++ xUnit/src/main/java/dev/banik/xunit/WasRun.java
@@ -2,7 +2,6 @@ package dev.banik.xunit;
 
 class WasRun extends TestCase {
 
-    public boolean wasRun = false;
     public String log = "";
 
     public WasRun(String methodName) {
@@ -11,7 +10,6 @@ class WasRun extends TestCase {
 
     @Override
     protected void setUp() {
-        this.wasRun = false;
         this.log = "setUp ";
     }
 
@@ -21,7 +19,6 @@ class WasRun extends TestCase {
     }
 
     public void testMethod() {
-        wasRun = true;
         this.log = this.log + "testMethod ";
     }
 
